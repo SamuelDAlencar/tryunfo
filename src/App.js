3b928c36b518864c4ae3c5e8e398b8a226e6fe7d
SamuelDAlencar <sarl.2001@hotmail.com>
2022-01-10 11:59:11 -0300

    requisition 5 conclusion

--- src/App.js
+++ src/App.js
@@ -23,6 +23,34 @@ class App extends React.Component {
   onInputChange({ target: { name, type, checked, value } }) {
     this.setState({
       [name]: type === 'checkbox' ? checked : value,
+    }, () => {
+      const {
+        cardName,
+        cardDescription,
+        cardAttr1,
+        cardAttr2,
+        cardAttr3,
+        cardImage,
+        cardRare,
+      } = this.state;
+      const maxAttr = 210;
+      const minAttr = 90;
+
+      if (cardName !== ''
+        && cardDescription !== ''
+        && cardImage !== ''
+        && cardRare !== ''
+        && Number(cardAttr1) + Number(cardAttr2) + Number(cardAttr3) <= maxAttr
+        && Number(cardAttr1) <= minAttr
+        && Number(cardAttr2) <= minAttr
+        && Number(cardAttr3) <= minAttr
+        && Number(cardAttr1) >= 0
+        && Number(cardAttr2) >= 0
+        && Number(cardAttr3) >= 0) {
+        this.setState({ isSaveButtonDisabled: false });
+      } else {
+        this.setState({ isSaveButtonDisabled: true });
+      }
     });
   }
 
