08a5b6033c13edf11d1ae2ca01f184aba4cc6365
SamuelDAlencar <sarl.2001@hotmail.com>
2022-01-11 16:20:05 -0300

    eighth requisition

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './components/Form';
 import Card from './components/Card';
+// import AddCard from './components/AddCard';
 
 class App extends React.Component {
   constructor() {
@@ -8,6 +9,7 @@ class App extends React.Component {
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
     this.state = {
+      cards: [],
       cardName: '',
       cardDescription: '',
       cardAttr1: '0',
@@ -77,8 +79,8 @@ class App extends React.Component {
       cardRare,
     };
 
-    this.setState({
-      [cardName]: newCard,
+    this.setState(({ cards }) => ({
+      cards: [...cards, newCard],
       cardName: '',
       cardDescription: '',
       cardAttr1: '0',
@@ -88,7 +90,7 @@ class App extends React.Component {
       cardRare: '',
       isSaveButtonDisabled: true,
       cardTrunfo: false,
-    });
+    }));
 
     if (cardTrunfo && !hasTrunfo) {
       this.setState({ hasTrunfo: true });
@@ -97,6 +99,7 @@ class App extends React.Component {
 
   render() {
     const {
+      cards,
       cardName,
       cardDescription,
       cardAttr1,
@@ -135,6 +138,17 @@ class App extends React.Component {
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {cards.map((card) => (<Card
+          key={ card.cardName }
+          cardName={ card.cardName }
+          cardDescription={ card.cardDescription }
+          cardAttr1={ card.cardAttr1 }
+          cardAttr2={ card.cardAttr2 }
+          cardAttr3={ card.cardAttr3 }
+          cardImage={ card.cardImage }
+          cardRare={ card.cardRare }
+          cardTrunfo={ card.cardTrunfo }
+        />))}
       </div>
     );
   }
